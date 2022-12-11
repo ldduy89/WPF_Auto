71887002a68bcce06d3790981116f70f1ce087ca
duyld <ldduy89@gmail.com>
2022-12-11 20:54:47 +0700

    v1v1

--- WPF_Auto/MainWindow.xaml.cs
+++ WPF_Auto/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -32,33 +33,24 @@ namespace WPF_Auto
         Bitmap CHROME_BMP;
         #endregion
         List<Charater> chars = new List<Charater>();
+        LDPlayer ldplayer = new LDPlayer();
         public MainWindow()
         {
             InitializeComponent();
             LoadData();
             LoadDevices();
-
         }
         void LoadData()
         {
             //CHROME_BMP = (Bitmap)Bitmap.FromFile("data//ss1.png");
 
+            LDPlayer.pathLD = "C:\\LDPlayer\\LDPlayer4.0\\ldconsole.exe";
             using (StreamReader sr = File.OpenText("./data.json"))
             {
                 var obj = sr.ReadToEnd();
                 chars = JsonConvert.DeserializeObject<List<Charater>>(obj);
                 lstChar.ItemsSource = chars;
             }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Task t = new Task(() =>
-            {
-
-            });
-            t.Start();
-
         }
 
         bool isStart = false;
@@ -124,70 +116,115 @@ namespace WPF_Auto
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            var listDevice = KAutoHelper.ADBHelper.GetDevices();
-            int index = 0;
-            if (listDevice.Count == 0) return;
             isStart = true;
+            var devices = ldplayer.GetDevices();
+            ldplayer.Open("name", devices[0]);
+            ldplayer.Open("name", devices[1]);
+            ldplayer.Open("name", devices[2]);
+            ldplayer.Open("name", devices[3]);
+            //foreach (var driver in devices)
+            //{
+            //    ldplayer.Open("name", driver);
+            //}
             foreach (Charater item in chars)
             {
                 if (item.isChecked)
                 {
-                    if (index < listDevice.Count)
-                    {
-                        item.status = "playing";
-                        item.deviceId = listDevice[index];
-                        Auto(item);
-                        index++;
-                    }
-                    else
-                    {
-                        item.status = "waiting";
-                    }
-
+                    item.status = "waiting";
                 }
-
             }
             CollectionViewSource.GetDefaultView(lstChar.ItemsSource).Refresh();
-            //checkDevice();
+            WaitRunDevices();
         }
 
-        private void checkDevice()
+        private void WaitRunDevices()
         {
             new Task(() =>
             {
-                while (true)
+                while (isStart)
                 {
-                    if (!isStart) return;
-                    Delay(2);
-                    var listDevice = KAutoHelper.ADBHelper.GetDevices();
-                    List<Charater> _chars = chars.FindAll(c => c.isChecked && c.status == "playing");
-                    var drivers = listDevice.FindAll(d => chars.Find(c => c.deviceId == d) == null);
+                    var devicesRuning = ldplayer.GetDevices2_Running();
+                    List<string> devices = new List<string>();
+
+                    foreach (var device in devicesRuning)
+                    {
+                        Charater _char = chars.Find(c => c.deviceName == device.name);
+                        if (_char == null)
+                        {
+                            Charater _charWaiting = chars.Find(c => c.isChecked && c.status == "waiting");
+                            if (_charWaiting != null)
+                            {
+                                _charWaiting.status = "playing";
+                                _charWaiting.deviceId = device.adb_id;
+                                _charWaiting.deviceName = device.name;
+                                Auto(_charWaiting);
+
+                            }
+                        }
+                    };
+
+                    List<Charater> _charsDrop = chars.FindAll(c => c.deviceName != null && devicesRuning.Find(d => d.name == c.deviceName).name == null);
+                    if (_charsDrop.Count > 0)
+                    {
+                        foreach(Charater _char in _charsDrop)
+                        {
+                            _char.status = "";
+                            _char.deviceId = null;
+                            _char.deviceName = null;
+                            _char.isChecked = false;
+                        }
+
+                    }
+
+                    Application.Current.Dispatcher.Invoke(() => {
+                        CollectionViewSource.GetDefaultView(lstChar.ItemsSource).Refresh();
+                    });
+                    Delay(3);
                 }
             }).Start();
         }
+        
+        //private void checkDevice()
+        //{
+        //    new Task(() =>
+        //    {
+        //        while (true)
+        //        {
+        //            if (!isStart) return;
+        //            Delay(2);
+        //            var listDevice = KAutoHelper.ADBHelper.GetDevices();
+        //            List<Charater> _chars = chars.FindAll(c => c.isChecked && c.status == "playing");
+        //            var drivers = listDevice.FindAll(d => chars.Find(c => c.deviceId == d) == null);
+        //        }
+        //    }).Start();
+        //}
 
         private void Auto(Charater item)
         {
-            Task t = new Task(() =>
+            new Task(() =>
             {
-                Delay(5);
-                if (!isStart) return;
+                Delay(10);
+                ldplayer.RunApp("name", item.deviceName, "com.kv1.mobi");
+                Delay(60);
+                ldplayer.KillApp("name", item.deviceName, "com.kv1.mobi");
+                //if (!isStart) return;
+                //Charater _char = chars.Find(c => c.isChecked && c.status == "waiting");
+                //if (_char != null)
+                //{
+                //    _char.status = "playing";
+                //    _char.deviceId = item.deviceId;
+                //    _char.deviceName = item.deviceName;
+                //}
+                item.status = "";
+                item.deviceId = null;
+                item.deviceName = null;
+                item.isChecked = false;
                 Application.Current.Dispatcher.Invoke(() =>
                 {
-                    Charater _char = chars.Find(c => c.isChecked && c.status == "waiting");
-                    if (_char != null)
-                    {
-                        _char.status = "playing";
-                        _char.deviceId = item.deviceId;
-                        Auto(_char);
-                    }
-                    item.status = "";
-                    item.deviceId = "";
-                    item.isChecked = false;
                     CollectionViewSource.GetDefaultView(lstChar.ItemsSource).Refresh();
                 });
-            });
-            t.Start();
+                //Auto(_char);
+            }).Start();
         }
 
         class Charater
@@ -197,29 +234,23 @@ namespace WPF_Auto
             public bool isChecked { get; set; }
             public string status { get; set; }
             public string deviceId { get; set; }
+            public string deviceName { get; set; }
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            _ = chars;
             isStart = false;
             foreach (Charater item in chars)
             {
                 if (item.isChecked)
                 {
-
                     item.status = "";
-                    item.deviceId = "";
-
+                    item.deviceId = null;
+                    item.deviceName = null;
                 }
 
             }
             CollectionViewSource.GetDefaultView(lstChar.ItemsSource).Refresh();
-        }
-
-        private void lstChar_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
 
         private void CheckBox_Click(object sender, RoutedEventArgs e)
@@ -230,7 +261,6 @@ namespace WPF_Auto
             foreach (Charater item in chars)
             {
                 item.isChecked = isChecked;
-
             }
             CollectionViewSource.GetDefaultView(lstChar.ItemsSource).Refresh();
         }
